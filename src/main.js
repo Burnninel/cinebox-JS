9a9a88757a386840dc77afcf9ac578e8e9c9e09e
Burnninel <brunoabc11@gmail.com>
2025-07-15 20:47:31 -0300

    Componentiza SectionFilmes
    Move toda a seção de filmes para o componente SectionFilmes.js, incluindo header e lista de cards. Mantém o HTML base simples com apenas a tag <main class='app'> para injeção dinâmica via JS.

--- src/main.js
+++ src/main.js
@@ -1,47 +1,6 @@
 import { Header } from "/src/components/Header.js";
-import { CardItem } from "/src/components/Card.js";
+import { SectionFilmes } from "./components/SectionFilmes.js";
 
 document.body.insertAdjacentHTML("afterbegin", Header());
 
-const cards = [
-  {
-    src: "assets/img/deadpool.jpg",
-    alt: "banner_deadpool",
-    title: "Deadpool & Wolverine",
-    genre: "Ação",
-    year: "2023",
-    score: "4,5",
-  },
-  {
-    src: "assets/img/mmf.jpg",
-    alt: "banner_meu_malvado_favorito4",
-    title: "Meu Malvado Favorito 4",
-    genre: "Animação",
-    year: "2024",
-    score: "4,9",
-  },
-  {
-    src: "assets/img/deadpool.jpg",
-    alt: "banner_deadpool",
-    title: "Deadpool & Wolverine",
-    genre: "Ação",
-    year: "2023",
-    score: "4,5",
-  },
-  {
-    src: "assets/img/mmf.jpg",
-    alt: "banner_meu_malvado_favorito4",
-    title: "Meu Malvado Favorito 4",
-    genre: "Animação",
-    year: "2024",
-    score: "4,9",
-  }
-];
-
-const html = cards
-  .map((card) => CardItem(card))
-  .join("");
-
-document
-  .querySelector(".explore__card-list")
-  .insertAdjacentHTML("afterbegin", html);
+document.querySelector(".app").insertAdjacentHTML("afterbegin", SectionFilmes('Explorar'));
